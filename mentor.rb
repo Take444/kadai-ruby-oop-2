5d3ba6d109cc8116d6701071e5c45a4b87fec386
takehiro.nakai <tn.44.formal@gmail.com>
2024-01-24 21:55:43 +0000

    kadai-ruby-oop-2

--- mentor.rb
+++ mentor.rb
@@ -2,33 +2,25 @@ class Mentor
   # インスタンス変数
   attr_accessor :name
   
-  def initialize
-    self.name = name
+  def initialize(name)
+    @name = name
   end
   
   def job
     puts "#{self.name}です。私は現役のITプロフェッショナルです。"
   end
-  
-  def name
-    @@name ='煌木'
-  end
-  
+
 end
 
 class RailsMentor < Mentor
   def job
     puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
   end
-
-  def name
-    @@name ='赤出'
-  end
   
 end
 
-kirameki = Mentor.new
-akaide = RailsMentor.new
+kirameki = Mentor.new("煌木")
+akaide = RailsMentor.new("赤出")
 
 kirameki.job
 akaide.job
